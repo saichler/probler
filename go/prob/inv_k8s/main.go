64fc52288f19050e7417071128f2f1495a30a7b8
Sharon Aicler <saichler@gmail.com>
2025-04-21 18:58:56 -0700

    Add k8s collector

--- go/prob/inv_k8s/main.go
+++ go/prob/inv_k8s/main.go
@@ -13,18 +13,25 @@ import (
 )
 
 func main() {
-	resources := common2.CreateResources("vnic-" + os.Getenv("HOSTNAME"))
+	res := common2.CreateResources("k8s-" + os.Getenv("HOSTNAME"))
+	res.Logger().Info("Starting k8s")
 	common.SetNetworkMode(common.NETWORK_K8s)
-	nic := vnic.NewVirtualNetworkInterface(resources, nil)
+	nic := vnic.NewVirtualNetworkInterface(res, nil)
 	nic.Start()
+	nic.WaitForConnection()
+	res.Logger().Info("Registering k8s service")
+	//Add the inventory model and mark the Id field as key
+	clusterNode, _ := nic.Resources().Introspector().Inspect(&types2.Cluster{})
+	introspecting.AddPrimaryKeyDecorator(clusterNode, "Name")
 
-	//Add the k8s cluster model and mark the Name as key
-	k8sClusterNode, _ := nic.Resources().Introspector().Inspect(&types2.Cluster{})
-	introspecting.AddPrimaryKeyDecorator(k8sClusterNode, "Name")
-
-	//Activate the k8s inventory service with the primary key & sample model instance
-	nic.Resources().ServicePoints().Activate(inventory.ServicePointType, common2.INVENTORY_SERVICE_K8S, common2.INVENTORY_AREA_K8S, nic.Resources(),
+	//Activate the box inventory service with the primary key & sample model instance
+	res.ServicePoints().AddServicePointType(&inventory.InventoryServicePoint{})
+	_, err := nic.Resources().ServicePoints().Activate(inventory.ServicePointType, common2.INVENTORY_SERVICE_K8S, common2.INVENTORY_AREA_K8S, nic.Resources(),
 		nic, "Name", &types2.Cluster{}, &types.DeviceServiceInfo{ServiceName: common2.ORM_SERVICE, ServiceArea: 0})
+
+	if err != nil {
+		res.Logger().Error(err)
+	}
 
 	common2.WaitForSignal(nic.Resources())
 }
