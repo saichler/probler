3ae1f040d8f1e4d3560e550f326a8c5cf3a195bd
Sharon Aicler <saichler@gmail.com>
2025-09-03 17:07:04 -0700

    Add support for topology model

---
+++ k8s/apply-all.sh
@@ -0,0 +1,14 @@
+kubectl apply -f vnet.yaml
+sleep 5
+#pod-logs.sh vnet
+kubectl apply -f parser.yaml
+sleep 2
+kubectl apply -f collector.yaml
+sleep 2
+kubectl apply -f box.yaml
+sleep 2
+kubectl apply -f k8s.yaml
+sleep 2
+kubectl apply -f orm.yaml
+sleep 2
+kubectl apply -f webui.yaml
