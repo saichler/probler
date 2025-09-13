258b6c5f799ea92d2a28537200af408bb99fd444
Sharon Aicler <saichler@gmail.com>
2025-09-13 06:11:10 -0700

    add 10k

--- go/prob/prctl/commands/addDevices.go
+++ go/prob/prctl/commands/addDevices.go
@@ -99,6 +99,20 @@ func AddDevices(cmd string, rc *client.RestClient, resources common2.IResources)
 		}
 	}
 
+	if cmd == "10K" {
+		ip := 1
+		sub := 40
+		for i := 1; i <= 10000; i++ {
+			device := creates.CreateDevice("60.50."+strconv.Itoa(sub)+"."+strconv.Itoa(ip), 0)
+			deviceList.List = append(deviceList.List, device)
+			ip++
+			if ip > 254 {
+				sub++
+				ip = 0
+			}
+		}
+	}
+
 	if len(deviceList.List) == 0 {
 		fmt.Println("No devices in list")
 		return
