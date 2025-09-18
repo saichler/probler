b45173b5fefc9b931dfc0dfd5d45b3d89faaa352
Sharon Aicler <saichler@gmail.com>
2025-09-18 11:53:07 -0700

    update prctl

--- go/prob/prctl/commands/addDevices.go
+++ go/prob/prctl/commands/addDevices.go
@@ -134,7 +134,7 @@ func AddDevices(cmd string, rc *client.RestClient, resources common2.IResources)
 
 	fmt.Println("Adding ", len(deviceList.List), " devices")
 
-	resp, err := rc.POST("0/"+targets.ServiceName, "L8TargetList", "", "", deviceList)
+	resp, err := rc.POST("0/"+targets.ServiceName, "L8C_TargetList", "", "", deviceList)
 	if err != nil {
 		fmt.Println("Error", err)
 	}
