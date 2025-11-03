db8ec0d0210e2eeea153800b3335008314443b80
Sharon Aicler <saichler@gmail.com>
2025-11-03 06:38:54 -0800

    add logs vnet

--- go/prob/log-vnet/build.sh
+++ go/prob/log-vnet/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-docker build --no-cache --platform=linux/amd64 -t saichler/log-vnet:latest .
-docker push saichler/log-vnet:latest
+docker build --no-cache --platform=linux/amd64 -t saichler/logs-vnet:latest .
+docker push saichler/logs-vnet:latest
