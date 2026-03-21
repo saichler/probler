cc21ab9e365868c0a281be1b5ec578a356e4096f
Sharon Aicler <saichler@gmail.com>
2026-03-21 03:25:49 -0700

    Fix local

--- go/run-demo.sh
+++ go/run-demo.sh
@@ -12,7 +12,7 @@ if [ -z "$1" ]; then
   nohup ./vnet_demo > vnet_demo.log 2>&1 &
   nohup ./logvnet_demo > logvnet_demo.log 2>&1 &
   sleep 5
-  nohup ./orm_demo > orm_demo.log 2>&1 &
+  nohup ./orm_demo local > orm_demo.log 2>&1 &
   nohup ./alarms_demo > alarms_demo.log 2>&1 &
   nohup ./collector_demo > collector_demo.log 2>&1 &
   nohup ./parser_demo > parser_demo.log 2>&1 &
